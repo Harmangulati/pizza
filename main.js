0011d3852a47eabf08f15bda60b9561a9247fed5
Harmangulati <83772120+Harmangulati@users.noreply.github.com>
2021-07-14 08:55:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,15 +10,29 @@ for(var i=0;<menu_list_array.length;i++){
     htmldata=htmldata+ '<li>' +menu_list_array[i] + '<li>'
 }
 htmldata=htmldata+"</ol>"
+document.getElementById("display_menu").innerhtml=htmldata;
 }
 
 function add_item(){
 var htmldata;
 var item=document.getElementById("add_item").value;
 //Complete the code
-
+var htmldata;
+var item=document.getElementById("add_item").value;
+menu_list_array.sort();
+htmldata="<section calss='cards'>"
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+'<div class="card">'
+    +<img src="pizza.png"/>
++menu_list_array[i] + '</div>'
+htmldata=htmldata+"</section>"
+document.getElementById("display_addedmenu"),innerhtml=htmldata;
+}
 }
 
 function add_top(){
 //Complete the code
+var item=document.getElementById("add_item").value
+menu_list_array.push(item);
+add_item();
 }
